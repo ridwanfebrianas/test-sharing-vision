5bda1013d2c320cf9769c3bf69481b099f780ff8
ridwanfebrianas <ridwanfebrianas@gmail.com>
2021-05-06 22:32:46 +0700

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './scss/style.scss';
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+// Containers
+const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'));
+const Register = React.lazy(() => import('./views/pages/register/Register'));
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+const UserManagement = React.lazy(() => import('./views/pages/user-management/UserManagement'));
+
+class App extends Component {
+  state = {
+    data : []
+  }
+
+  componentDidMount(){
+    const urlFetch = fetch('http://localhost:8080/user')
+    urlFetch.then( res => {
+       if(res.status === 200)
+          return res.json()   
+    }).then( resJson => {
+       this.setState({
+           data: resJson
+       })
+    })
+ }
+
+  render() {
+    return (
+      <HashRouter>
+          <React.Suspense fallback={loading}>
+            <Switch>
+              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+              <Route exact path="/usermanagement" name="User Management" render={props => <UserManagement {...props}/>} />
+              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
+            </Switch>
+          </React.Suspense>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
